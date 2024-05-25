bb836856692678e7c68b521b68b0edfe79d258d1
FelipeGabriel7 <felipegabfd@gmail.com>
2024-05-25 00:39:51 -0300

    routes: new route get unique users

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -13,6 +13,24 @@ def create_user(user: Users):
     database.append(user_id)
     return user_id
 
+@app.get('/users/{user_id}' , status_code=HTTPStatus.OK , response_model=UserReturn)
+def return_unique_user(user_id: int):
+    for dbUser in database:
+        
+        if user_id < 0 or user_id > len(database):
+              raise HTTPException(
+                status_code=HTTPStatus.NOT_FOUND, message='usuário não encontrado'
+            )
+    
+        if dbUser.id == user_id:
+                return dbUser
+        else:
+            raise HTTPException(
+                status_code=HTTPStatus.NOT_FOUND, message='usuário não encontrado'
+            )
+        
+    return {"user": dbUser}
+
 
 @app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
 def return_users():
@@ -27,7 +45,7 @@ def update_users(user_id: int, user: Users):
 
     if user_id > len(database) or user_id < 1:
         raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+            status_code=HTTPStatus.NOT_FOUND, detail='Usuário não encontrado'
         )
 
     database[user_id - 1] = user_with_id
@@ -38,7 +56,7 @@ def update_users(user_id: int, user: Users):
 def deleted_users(user_id: int):
     if user_id > len(database) or user_id < 1:
         raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+            status_code=HTTPStatus.NOT_FOUND, message='Usuário não encontrado'
         )
 
     del database[user_id - 1]
--- tests/tests_app.py
+++ tests/tests_app.py
@@ -57,12 +57,36 @@ def test_update_user(client):
         'email': 'bob@example.com',
         'id': 1,
     }
-
-
+    
 def test_delete_user(cliente):
     response = client.delete('/users/1')
 
     assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'message': 'Usuário Deletado com sucesso',
+        'status': 200,
+    }
+
+
+def test_update_user_error(client):
+    response = client.put(
+        '/users/100',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+    
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {
+        "message": "usuário não encontrado"
+    }
+
+def test_delete_user_error(client):
+    response = client.delete('/users/100')
+    
+    assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json() == {
         'message': 'Usuário Deletado com sucesso',
         'status': 200,
